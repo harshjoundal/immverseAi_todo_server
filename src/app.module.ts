776001a946c124d16e76da73e81714f02c0cc1b5
harshjoundal <harshjoundal@gmail.com>
2023-09-28 13:57:53 +0530

    removed unused routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,10 @@ import { AppService } from './app.service';
 import {config} from "./config/config"
 
 import {MongooseModule} from "@nestjs/mongoose"
-import { cityModule } from './modules/cities/cities.module';
-import { usersModule } from './modules/users/users.module';
 import { userModule } from './modules/user/user.module';
 import { todoModule } from './modules/todos/todo.module';
 @Module({
   imports: [MongooseModule.forRoot(config.mongo_url),
-  cityModule,
-  usersModule,
   userModule,
   todoModule
 ],
